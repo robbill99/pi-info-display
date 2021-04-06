afe34b4c4792594ff750c3f71bcc5784fe7dce3e
Rob Billington <robbill99@gmail.com>
2021-04-06 23:32:09 +0100

    hg

--- scrape.py
+++ scrape.py
@@ -54,7 +54,7 @@ def get_data():
     icon_details = content.find("img")
     icon_url = icon_details.attrs["src"]
 
-    weather_icon_data = requests.get(icon_url)
+    weather_icon_data = requests.get(icon_url).content
 
     forecast = content.find("div", {"class": "forecastText"})
     forecast = forecast.text
